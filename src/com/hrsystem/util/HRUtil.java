1c60aed08b8180fa664b9405a1dbd1e6762baec1
Satyanarayan <satyaclub@gmail.com>
2016-04-16 16:42:28 +0530

    Common Utility file
    For all unique members and methods.

---
+++ src/com/hrsystem/util/HRUtil.java
@@ -0,0 +1,32 @@
+package com.hrsystem.util;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class HRUtil {
+
+	/* Common Utility Fields */
+	public static final String DATE_FORMAT = "yyyy/MM/dd HH:mm:ss";
+
+	public class Action {
+		public static final String ADD = "add";
+		public static final String LIST = "list";
+		public static final String VIEW = "view";
+		public static final String EDIT = "edit";
+		public static final String UPDATE = "update";
+		public static final String DELETE = "delete";
+		public static final String REMOVE = "remove";
+	}
+
+	/* Common Utility Methods */
+	public static String getCurrentDateTime() {
+		DateFormat dateFormat = new SimpleDateFormat(HRUtil.DATE_FORMAT);
+
+		// get current date time with Calendar()
+		Calendar cal = Calendar.getInstance();
+		System.out.println(dateFormat.format(cal.getTime()));
+		return dateFormat.format(cal.getTime());
+	}
+
+}
